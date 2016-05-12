b3645b5a35f4f61f58b275be4acf15484132e20f
vikicandra <vikicandrata13@gmail.com>
2016-05-12 19:54:38 +0700

    dosen, mhs

---
+++ src/main/java/com/psbk/ws/Dosen.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.psbk.ws;
+
+/**
+ *
+ * @author VIKI
+ */
+public class Dosen {
+    private String id_dosen;
+    private String nama_dosen;
+    private String tglLahir;
+    private String noTelp;
+
+    public String getNoTelp() {
+        return noTelp;
+    }
+
+    public void setNoTelp(String noTelp) {
+        this.noTelp = noTelp;
+    }
+
+    public String getId_dosen() {
+        return id_dosen;
+    }
+
+    public void setId_dosen(String id_dosen) {
+        this.id_dosen = id_dosen;
+    }
+
+    public String getNama_dosen() {
+        return nama_dosen;
+    }
+
+    public void setNama_dosen(String nama_dosen) {
+        this.nama_dosen = nama_dosen;
+    }
+
+    public String getTglLahir() {
+        return tglLahir;
+    }
+
+    public void setTglLahir(String tglLahir) {
+        this.tglLahir = tglLahir;
+    }
+
+    @Override
+    public String toString() {
+        return "Data "+getNama_dosen();
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
--- src/main/java/com/psbk/ws/MahasiswaService.java
+++ src/main/java/com/psbk/ws/MahasiswaService.java
@@ -25,6 +25,8 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import static jdk.nashorn.internal.runtime.Debug.id;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -40,10 +42,8 @@ public class MahasiswaService extends MasterConnection{
     @Produces(MediaType.APPLICATION_JSON)
     public Map getMhsAll(){
         Map<String, Object> result = new HashMap<String, Object>();
-        
-        
-        try{
-           
+       
+        try{           
             createConnection();
             String sql = "select * from mahasiswa";
             List<MyMap> mhs = jt.queryList(sql, new MyMap());
@@ -91,130 +91,69 @@ public class MahasiswaService extends MasterConnection{
         return result;
     }
     
-    
-       
     @POST
+    @Path("/insertMatkul")
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("/listMhsPerwalian")
-    public Object getListMhsPerwalian(@Context HttpServletRequest hsr){
-        StringBuffer sb = new StringBuffer();
+    public Map insertMatkul(@Context HttpServletRequest hsr) throws JSONException{
+        Map response = new HashMap();
+        DataInputStream in;
         String line = null;
-        JSONObject request = null;
-        Map respon = new HashMap();
-        DataInputStream in;          
-
+        StringBuffer sb = new StringBuffer();
+        
         try{
             createConnection();
-            in = new DataInputStream(new BufferedInputStream(hsr.getInputStream()));
-
-            while((line = in.readLine()) != null)
-                sb.append(line);
-            JSONObject json = new JSONObject(sb.toString());
-            request = (JSONObject) json.get("request");
-
-            if(request == null){
-                respon.put("message", "Data yang dikirim tidak ditemukan");
-                respon.put("rCode", "404");
-                respon.put("statusId", "not found");
-                return respon;
-            }
-            String sql = "select M.nrp, M.nama from mahasiswa as M inner join perwalian as P "
-                       + "on M.nrp = P.nrp where id_dosen = ?";
-            String id = request.getString("id_dosen");
-            System.out.println(id);
-            List<Map> mhs = jt.queryList(sql, new Object[] {id},new MyMap());
-            closeConnection();
-
-            if(mhs != null){
-                respon.put("message", "Berhasi");
-                respon.put("rCode", "200");
-                respon.put("statusId", "ok");
-                respon.put("result", mhs);
-            }
-//                 
-        }catch(Exception e){
-               e.printStackTrace();
-               respon.put("message", e.getMessage());
-               respon.put("rCode", "99");
-               respon.put("statusId", "0");
-           }
-        return respon;
-    }
-    
-    @GET
-    @Path("/mkAll")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Map getMkAll(){
-        Map<String, Object> result = new HashMap<String, Object>();
-        try{
-            createConnection();
-            String sql = "select * from matakuliah";
-            List<MyMap> matkul = jt.queryList(sql, new MyMap());
             
-            if(matkul != null){
-                result.put("code", "200");
-                result.put("status", "ok");
-                result.put("message", "INQUIRY BERHASIL");
-                result.put("result", matkul);
-                
+            in = new DataInputStream(new BufferedInputStream(hsr.getInputStream()));
+            
+            while((line = in.readLine()) != null ){
+                sb.append(line);
+            }
+            JSONObject json = new JSONObject(sb.toString());
+            JSONObject request = (JSONObject) json.get("request");
+            
+            if(request == null){
+                response.put("status", "null");
+            }else{
+                response.put("status", "ada");
             }
         }catch(Exception e){
-            result.put("code", "404");
-            result.put("status", "not found");
-            result.put("message", "Gagal karena : "+e.getMessage());
+            response.put("status", e.getMessage());
         }
-        return result;
+        
+        return response;
     }
     
+    
     @POST
-    @Path("/detailMkMhs")
+    @Path("/coba")
     @Produces(MediaType.APPLICATION_JSON)
-    public Object getMkByNrp(@Context HttpServletRequest hsr){
+    public Object coba(@Context HttpServletRequest hsr){
+        JSONArray request = null;
         StringBuffer sb = new StringBuffer();
-        String line = null;
-        JSONObject request = null;
-        Map respon = new HashMap();
-        DataInputStream in;          
-
+        DataInputStream in;
+        MyMap respon = new MyMap(); 
+        String line;
+        
         try{
             createConnection();
             in = new DataInputStream(new BufferedInputStream(hsr.getInputStream()));
-
+            
             while((line = in.readLine()) != null)
                 sb.append(line);
+            
             JSONObject json = new JSONObject(sb.toString());
-            request = (JSONObject) json.get("request");
-
-            if(request == null){
-                respon.put("message", "Data yang dikirim tidak ditemukan");
-                respon.put("rCode", "404");
-                respon.put("statusId", "not found");
-                return respon;
+            request = (JSONArray) json.get("result");
+            
+            for (int i = 0; i < request.length() ; i++) {
+                System.out.println(i);
             }
-            String sql = "SELECT * FROM " +
-                        "matakuliah as M LEFT JOIN (detail_perwalian as DP join perwalian as P " +
-                        "ON DP.id_perwalian = P.id_perwalian and DP.id_perwalian = " +
-                        "(SELECT id_perwalian from perwalian where nrp = ?) )ON M.kode_mk = DP.kode_mk";
-            String nrp = request.getString("nrp");
-            System.out.println(nrp);
-            List<Map> mhs = jt.queryList(sql, new Object[] {nrp},new MyMap());
-            closeConnection();
-
-            if(mhs != null){
-                respon.put("message", "Berhasi");
-                respon.put("rCode", "200");
-                respon.put("statusId", "ok");
-                respon.put("result", mhs);
-            }
-//                 
+           
+            
         }catch(Exception e){
-               e.printStackTrace();
-               respon.put("message", e.getMessage());
-               respon.put("rCode", "99");
-               respon.put("statusId", "0");
-           }
+            respon.put("gagal : ", e.getMessage());
+        }
+        
         return respon;
     }
-    
     
 }
